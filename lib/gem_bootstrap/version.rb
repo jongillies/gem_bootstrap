500b1ccc8effb347ecc098509bfbaa60b672d7d0
Jon Gillies <jon@gillies.us>
2015-01-20 08:14:09 -0700

    Updated for ruby 2.2.0

--- lib/gem_bootstrap/version.rb
+++ lib/gem_bootstrap/version.rb
@@ -4,7 +4,7 @@ module GemBootstrap
   EMAIL = ['YOUR EMAIL ADDRESS HERE']
   DESCRIPTION = %q{This is a demonstration gem to show how to build and test gems}
   SUMMARY = %q{gem builder example}
-  HOMEPAGE = 'CODE REPOSITORY URL OR HOME PAGE URL'
+  HOMEPAGE = 'http://example.com'
   VERSION_BANNER = "#{self.name} #{self::VERSION} #{self::SUMMARY} (c) 2013 #{self::AUTHORS.join(', ')}"
   HELP_TEXT = "Use this program for GemBootstrap.\n\nUsage:"
 end
